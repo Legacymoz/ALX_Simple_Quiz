8bdf3b94738f9144bd806d8392f4a87b95ff00e8
Mozart <mozartkandie@gmail.com>
2024-10-03 14:51:04 +0300

    Done

---
+++ quiz.js
@@ -0,0 +1,17 @@
+function checkAnswer(){
+    const correctAnswer = "4";
+    const userAnswer=document.querySelector('input[name="quiz"]:checked').value;
+    const myFeedback= document.getElementById("feedback");
+
+    if (userAnswer == correctAnswer)
+    {
+        myFeedback.textContent= "Correct! Well done.";
+    }
+    else{
+        myFeedback.textContent= "That's incorrect. Try again!";
+    }
+}
+
+const submitButton=document.getElementById("submit-answer");
+
+submitButton.addEventListener("click", checkAnswer);
