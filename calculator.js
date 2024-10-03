caf447b17be07ba2f552994d5ea25c989e791d3d
Mozart <mozartkandie@gmail.com>
2024-10-03 16:03:39 +0300

    Calculator

---
+++ calculator.js
@@ -0,0 +1,43 @@
+const addBtn=document.getElementById('add');
+const subBtn=document.getElementById('subtract');
+const multBtn=document.getElementById('multiply');
+const divBtn=document.getElementById('divide');
+
+function add (num1, num2) {
+    return num1 + num2;
+}
+
+function sub (num1, num2) {
+    return num1 - num2;
+}
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+function divide (num1, num2) {
+    return num1 / num2;
+}
+
+addBtn.addEventListener('click',function(){
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = add(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+})
+subBtn.addEventListener('click',function(){
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = sub(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+})
+multBtn.addEventListener('click',function(){
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = multiply(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+})
+divBtn.addEventListener('click',function(){
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = divide(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+})
